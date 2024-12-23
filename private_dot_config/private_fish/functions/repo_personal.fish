c9d8794eebe6b842efaf5cb05a5395d0bedfa69a
Tyler Kindy <tylerkindy@gmail.com>
2024-12-23 16:18:52 -0500

    Don't track repo visits on personal computer

--- private_dot_config/private_fish/functions/repo_personal.fish
+++ private_dot_config/private_fish/functions/repo_personal.fish
@@ -1,14 +1,3 @@
 function repo
-  set repo_name $argv[1]
-  set repo_path $PROJECTS_HOME/$repo_name
-
-  if not test -d $repo_path
-    echo "Repo missing"
-    return 1
-  end
-
-  cd $repo_path
-
-  # Track when I visit each repo
-  date +%s >> .visited2
+  cd $PROJECTS_HOME/$argv[1]
 end
